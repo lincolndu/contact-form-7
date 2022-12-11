d236d7b0a2d8ccd02e93937b8f1e91a1111dfaff
Al Mahmud <lincolndu@yahoo.com>
2022-12-11 15:21:06 +0600

    wpcf7_plugin_path( 'block-editor' ) line 42.
    wpcf7_plugin_path( 'block-editor' ) arise a Notice: Function WP_Block_Type_Registry::register was called incorrectly. Block type names must contain a namespace prefix. Example: my-plugin/my-custom-block-type Please see Debugging in WordPress for more information. (This message was added in version 5.0.0.) in wp-includes/functions.php on line 5835
    replace wpcf7_plugin_path( 'block-editor' ) this with 'contact-form-7/contact-form-selector' with this code. 
    
    new code
    register_block_type(
    		'contact-form-7/contact-form-selector',
    		array(
    			'editor_script' => 'contact-form-7-block-editor',
    		)
    	);
    
    Previous code 
    register_block_type(
    		wpcf7_plugin_path( 'includes/block-editor' ),
    		array(
    			'editor_script' => 'contact-form-7-block-editor',
    		)
    	);

--- includes/block-editor/block-editor.php
+++ includes/block-editor/block-editor.php
@@ -37,9 +37,15 @@ function wpcf7_init_block_editor_assets() {
 		'contact-form-7-block-editor',
 		'contact-form-7'
 	);
-
+	
+	/**
+	 * wpcf7_plugin_path( 'block-editor' ) arise a Notice: Function WP_Block_Type_Registry::register was called incorrectly. Block type names must contain a namespace prefix. Example: my-plugin/my-custom-block-type Please see Debugging in WordPress for more information. (This message was added in version 5.0.0.) in wp-includes/functions.php on line 5835
+	 * replace wpcf7_plugin_path( 'block-editor' ) this with 'contact-form-7/contact-form-selector' with this code. 
+	 * @author lincolndu
+	*/
+	
 	register_block_type(
-		wpcf7_plugin_path( 'includes/block-editor' ),
+		'contact-form-7/contact-form-selector',
 		array(
 			'editor_script' => 'contact-form-7-block-editor',
 		)
